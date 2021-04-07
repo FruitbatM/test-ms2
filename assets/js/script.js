8c2b4f72264ed7ff37c71b9686319679546b1767
FruitbatM <mar.pavlovic@gmail.com>
2021-04-07 14:58:35 +0000

    Add close navigation function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,12 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+// Close navigation menu after clicking on anchor link
+let menuLinks = document.querySelectorAll('.menu a');
+
+menuLinks.forEach (
+  function(menuLink) {
+    menuLink.addEventListener('click', closeNav);
+  }
+);
